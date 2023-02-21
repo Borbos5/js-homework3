7a498a030db02fc535e5f836c3f015c5af020d72
Никита Баранов <124291854+Borbos5@users.noreply.github.com>
2023-02-21 16:13:11 +0200

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,18 @@
+// Роздруковуємо числа від 3 до 20
+for (let i = 3; i <= 20; i++) {
+    console.log(i);
+}
+
+// Роздруковуємо числа від 10 до 20
+for (let i = 10; i <= 20; i++) {
+    console.log(i);
+}
+
+// Просимо ввести 2 числа
+let startNum = parseInt(prompt("Введіть початкове число:"));
+let endNum = parseInt(prompt("Введіть кінцеве число:"));
+
+// Роздруковуємо числа між введеними числами
+for (let i = startNum; i <= endNum; i++) {
+    console.log(i);
+}
