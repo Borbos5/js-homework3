d661403c6cc5e4857544da9d5c4d5850ddb48d08
Никита Баранов <124291854+Borbos5@users.noreply.github.com>
2023-02-21 16:25:07 +0200

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,18 @@
+
+for (let i = 3; i <= 20; i++) {
+    console.log(i);
+}
+
+
+for (let i = 10; i <= 20; i++) {
+    console.log(i);
+}
+
+
+let startNum = (prompt("Введіть число:"));
+let endNum = (prompt("Введіть число:"));
+
+
+for (let i = startNum; i <= endNum; i++) {
+    console.log(i);
+}
